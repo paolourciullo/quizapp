e938c1864ef4c024fb08132452e2bf749d544e8f
Paolo U <paolourciullo@gmail.com>
2021-05-04 12:59:33 -0400

    add SW q's

--- script.js
+++ script.js
@@ -1,99 +1,99 @@
 const quizData = [
   {
-    question: 'question 1',
+    question:
+      ' In Star Wars, what do they call the invisible power that binds the galaxy together? ',
+    a: 'The Fourth',
+    b: 'The Force',
+    c: 'The Groove',
+    d: 'The Palpatine',
+    correct: 'b',
+  },
+  {
+    question: 'C-3P0 is fluent in how many languages?',
+    a: '280',
+    b: 'Over 60 million',
+    c: 'zero',
+    d: '38',
+    correct: 'b',
+  },
+  {
+    question: 'What is the name of Yoda’s home?',
     a: 'answer a',
-    b: 'answer b',
+    b: 'Dagobah',
     c: 'answer c',
     d: 'answer d',
     correct: 'd',
   },
   {
-    question: 'question 2',
+    question: 'Who built C-3P0?',
     a: 'answer a',
-    b: 'answer b',
-    c: 'answer c',
-    d: 'answer d',
-    correct: 'd',
-  },
-  {
-    question: 'question 3',
-    a: 'answer a',
-    b: 'answer b',
-    c: 'answer c',
-    d: 'answer d',
-    correct: 'd',
-  },
-  {
-    question: 'question 4',
-    a: 'answer a',
-    b: 'answer b',
+    b: 'Anakin Skywalker',
     c: 'answer c',
     d: 'answer d',
     correct: 'd',
   },
 ];
 
-const quiz = document.getElementById('quiz')
-const answerEls = document.querySelectorAll('.answer')
-const questionEl = document.getElementById('question')
-const a_text = document.getElementById('a_text')
-const b_text = document.getElementById('b_text')
-const c_text = document.getElementById('c_text')
-const d_text = document.getElementById('d_text')
-const submitBtn = document.getElementById('submit')
+const quiz = document.getElementById('quiz');
+const answerEls = document.querySelectorAll('.answer');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
 
-let currentQuiz = 0
+let currentQuiz = 0;
 
-let score = 0
+let score = 0;
 
-loadQuiz()
+loadQuiz();
 
 function loadQuiz() {
+  deselectAnswers();
+  const currentQuizData = quizData[currentQuiz];
 
-  deselectAnswers()
-  const currentQuizData = quizData[currentQuiz]
-
-  questionEl.innerText = currentQuizData.question
-  a_text.innerText = currentQuizData.a
-  b_text.innerText = currentQuizData.b
-  c_text.innerText = currentQuizData.c
-  d_text.innerText = currentQuizData.d
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
 }
 
 function deselectAnswers() {
-  answerEls.forEach(answerEl => answerEl.checked = false)
+  answerEls.forEach((answerEl) => (answerEl.checked = false));
 }
 
 function getSelected() {
-  let answer
+  let answer;
 
-  answerEls.forEach(answerEl => {
-    if(answerEl.checked) {
-      answer = answerEl.id
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
     }
-  })
+  });
 
-  return answer
+  return answer;
 }
 
 submitBtn.addEventListener('click', () => {
-  const answer = getSelected()
-  
-  if(answer) {
-    if(answer === quizData[currentQuiz].correct) {
-      score++
+  const answer = getSelected();
+
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
     }
 
-    currentQuiz++
+    currentQuiz++;
 
-    if(currentQuiz < quizData.length) {
-      loadQuiz()
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
     } else {
       quiz.innerHTML = `
         <h2>You answered correctly at ${score} / ${quizData.length} questions correctly!</h2>
 
         <button onclick="location.reload()">Reload</button>
-      `
+      `;
     }
   }
-})
+});
