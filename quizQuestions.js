1193fbb42ef75b3747bc4d840718668d84ccb10b
Paolo U <paolourciullo@gmail.com>
2021-05-15 10:35:38 -0400

    external quiz data

---
+++ quizQuestions.js
@@ -0,0 +1,93 @@
+const quizData = [
+  {
+    question:
+      'Which HTML5 element should contain important links for navigating a website?',
+    a: '<li>',
+    b: '<ul>',
+    c: '<header>',
+    d: '<nav>',
+    correct: 'd',
+  },
+  {
+    question:
+      'Which HTML5 element should contain important information about what page you are on and the topic of the page?',
+    a: '<li>',
+    b: '<ul>',
+    c: '<header>',
+    d: '<nav>',
+    correct: 'c',
+  },
+  {
+    question:
+      'Which JavaScript variable declaration is used when the assigned value is likely to change?',
+    a: 'let',
+    b: 'var',
+    c: 'variable',
+    d: 'const',
+    correct: 'a',
+  },
+  {
+    question:
+      'Which term in JavaScript can be used to describe a declared variable that has not yet been given a value?',
+    a: 'null',
+    b: 'undefined',
+    c: 'typeError',
+    d: 'NaN',
+    correct: 'b',
+  },
+  {
+    question:
+      "Which selector could refer to an html element with the class of 'sm-col'?",
+    a: '#sm-col',
+    b: '.sm, .col',
+    c: '.sm-col',
+    d: '.sm_col',
+    correct: 'c',
+  },
+  {
+    question:
+      "Setting an element height to '10vh' bases the height calculation on _________'",
+    a: 'viewfinder height',
+    b: 'viewport width',
+    c: 'visible height',
+    d: 'viewport height',
+    correct: 'd',
+  },
+  {
+    question: 'Which HTML5 property can be used for custom data?',
+    a: 'data-',
+    b: '-data-',
+    c: 'data=',
+    d: 'custom',
+    correct: 'a',
+  },
+  {
+    question:
+      "Which HTML5 tag is used to define 'independant, self-contained content' within a web page?",
+    a: '<section>',
+    b: '<main>',
+    c: '<aside>',
+    d: '<article>',
+    correct: 'd',
+  },
+  {
+    question:
+      'Which of the following snippets does not result in a return value of 8?',
+    a: '8**1',
+    b: 'Math.pow(16, .75)',
+    c: "2 * 2 * 'two'",
+    d: "2**+'3'",
+    correct: 'c',
+  },
+  {
+    question:
+      'Which of these statements is NOT true of browser-side JavaScript?',
+    a: 'JavaScript can be used for functional programming.',
+    b: 'JavaScript can be used for file reading and writing on client machines.',
+    c: 'JavaScript does not require frameworks or libraries to be used.',
+    d: 'Though not class-based, JavaScript is an Object-Oriented Programming Language.',
+    correct: 'b',
+  },
+];
+
+export default quizData;
